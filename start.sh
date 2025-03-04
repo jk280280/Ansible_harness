2e5e05e73fd23e6496827cc9dac71cc5c330dc18
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-03-04 12:57:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,10 @@ echo "Starting Harness Delegate with Ansible..."
 service ssh start
 
 # Run Ansible to configure EKS nodes
-ansible-playbook /etc/ansible/playbooks/setup.yml
+ansible-playbook /etc/ansible/playbooks/setup.yml || {
+  echo "Setup failed, rolling back..."
+  ansible-playbook /etc/ansible/playbooks/rollback.yml
+}
 
 # Start Harness Delegate
 exec /harness/start.sh
